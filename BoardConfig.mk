aa9e87f2f3e77404d1742b8be2ab8592ae01220e
noiphonehere <noiphonehere@gmail.com>
2013-06-17 11:30:17 -0400

    Override libcamera with Max's fixed version.
    Change-Id: I0b9977d6d89aea01ec9f229e89d4f5941529f590

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,5 +48,8 @@ BOARD_CUSTOM_BOOTIMG_MK := device/samsung/galaxys4gmtd/shbootimg.mk
 
 TARGET_OTA_ASSERT_DEVICE := aries,galaxys4g,galaxys4gmtd,SGH-T959V,SGH-T959W
 
+# Camera
+TARGET_PROVIDES_LIBCAMERA := true
+
 # Import the aries-common BoardConfigCommon.mk
 include device/samsung/aries-common/BoardConfigCommon.mk
